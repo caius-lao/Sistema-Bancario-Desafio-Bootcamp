2e9d52d316f1fbd71e70c26b31a26ba085cad494
Caius Lao <86391537+caius-lao@users.noreply.github.com>
2023-07-27 23:56:50 -0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,27 @@
+from sistema_banco import Banco
+
+class Menu:
+    banco = Banco(1000)
+    menu = """
+    Operações:
+    [d] Depósito
+    [s] Saque
+    [e] Extrato
+    [q] Sair
+
+    => """
+    
+    def main(self):
+        while True:
+            op = input(self.menu)
+            match op:
+                case 'd':
+                    self.banco.deposito(float(input("Insira o valor para depósito: ")))
+                case 's':
+                    self.banco.saque(float(input("Insira o valor para saque: ")))
+                case 'e':
+                    self.banco.extrato()
+                case 'q':
+                    break
+                case _:
+                    print("Opção inválida!")
